0dce85003414a86eebea62564870b1a4dec0485f
DevKittyJS <devkittyjs@gmail.com>
2025-12-18 19:56:19 +0000

    Fix Express 5 wildcard route

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/api/icons", async (req, res) => {
 });
 
 // SPA fallback
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
